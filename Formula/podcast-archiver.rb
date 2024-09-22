dcc379da7d2f2c7d2096c012a5caf510bfe0cdfc
janw <janw@users.noreply.github.com>
2024-09-22 08:25:56 +0000

    bump(podcast-archiver): v1.5.2

--- Formula/podcast-archiver.rb
+++ Formula/podcast-archiver.rb
@@ -6,8 +6,8 @@ class PodcastArchiver < Formula
   license "MIT"
   head "https://github.com/janw/podcast-archiver.git", branch: "main"
 
-  url "https://files.pythonhosted.org/packages/82/e8/cc5b599cb57dcb29187310391d438624511e2fd60c16034e21bee3680a63/podcast_archiver-1.5.0.tar.gz"
-  sha256 "a31d952ec5ffc0ff1debee78fbfdb136330d32eb0dab0c5ebaca9ca83cf74a64"
+  url "https://files.pythonhosted.org/packages/5f/ce/dae40f5888bef9cb4eb183e089e17d5b9a4d2b8210147c8277a0fa6e9b7f/podcast_archiver-1.5.2.tar.gz"
+  sha256 "14278cb5ceecacbd3fe31fe958b4d54db3211ea010d6638f24239621a2c00b70"
 
   depends_on "python@3.12"
 
